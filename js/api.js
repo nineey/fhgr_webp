5f0e8efe1d00d23b6d7f1a57db8878416aa7bd49
nineey <56111891+nineey@users.noreply.github.com>
2021-10-24 15:02:34 +0200

    add comment for full data set

--- js/api.js
+++ js/api.js
@@ -1,5 +1,8 @@
 const api_url = "https://data.nicolasneeser.ch/netflix_data_short.json";
 
+// full data set
+// const api_url = "https://data.nicolasneeser.ch/netflix_data_all.json";
+
 // Fetch the data via API
 async function getNetflixData() {
   // Get data as a very long string
