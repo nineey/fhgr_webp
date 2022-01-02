4c2090be46ed42c927d52ac3dc8265ce35cd57f2
nineey <56111891+nineey@users.noreply.github.com>
2022-01-02 20:30:00 +0100

    adjust footer

--- client/src/components/container/Footer.js
+++ client/src/components/container/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Github, PlugFill } from "react-bootstrap-icons";
 
 export default function Footer() {
   return (
@@ -7,9 +8,30 @@ export default function Footer() {
       <StyledFooter>
         <footer>
           <div className="container pt-3">
-            <p>
-              Powered by <b>React</b> and <b>Express</b>
-            </p>
+            <div className="row">
+              <div className="col-12 col-md-6">
+                <p className="poweredBy">
+                  Handcrafted by <b>DBM students </b>
+                  <a
+                    rel="noreferrer noopener"
+                    target="_blank"
+                    href="https://github.com/nineey/fhgr_webp"
+                  >
+                    <StyledIcon>
+                      <Github />
+                    </StyledIcon>
+                  </a>
+                </p>
+              </div>
+              <div className="col d-flex justify-content-end">
+                <p className="madeBy d-none d-lg-block">
+                  Powered by <b>React</b> and <b>Express </b>
+                  <StyledIcon>
+                    <PlugFill />
+                  </StyledIcon>
+                </p>
+              </div>
+            </div>
           </div>
         </footer>
       </StyledFooter>
@@ -20,16 +42,27 @@ export default function Footer() {
 const StyledFooter = styled.div`
   footer {
     background-color: var(--secondary-color);
-    height: 3.5em;
+    height: 3.7em;
     bottom: 0;
     margin-top: 4.8em;
   }
 
-  p {
+  .poweredBy {
     color: var(--primary-color);
+    margin-top: -4px;
+  }
+
+  .madeBy {
+    color: var(--primary-color);
+    margin-top: -4px;
   }
 
   @media (max-width: 1030px) {
     margin-top: 7em;
   }
+`;
+
+const StyledIcon = styled.span`
+  font-size: 1.3em;
+  color: var(--primary-color);
 `;
